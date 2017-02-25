a661f8e956b06a72f0170428188f4accc7f0266c
Parra, Justin <parra.justin.main@gmail.com>
2017-02-25 15:19:30 -0700

    Added to spawn method as well as making team 1 move based on input

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,6 @@
 ﻿// using System.Collections.Generic;
 using UnityEngine;
-using System;
+// using System;
 
 public class GameController : MonoBehaviour {
 
@@ -69,34 +69,51 @@ public class GameController : MonoBehaviour {
 		floorArray[gameBoardSize - 1, 0].GetComponent<MeshRenderer>().material = Red;
 		floorArray[gameBoardSize - 1, gameBoardSize - 1].GetComponent<MeshRenderer>().material = Yellow;
 
-		// Now create the default Pieces and some random blocking pieces
+		// Now create the default Pieces
 		spawn();
+
+		// Spawn some barriers
+		spawn(4, Random.Range(1, gameBoardSize-1), Random.Range(1, gameBoardSize-1));
+		spawn(4, Random.Range(1, gameBoardSize-1), Random.Range(1, gameBoardSize-1));
+		spawn(4, Random.Range(1, gameBoardSize-1), Random.Range(1, gameBoardSize-1));
 	}
 
-	private void spawn(int x = -1, int y = -1) {
-		if( x == -1 && y == -1) { 
+	// Spawn something with a certain Team identifier
+	private void spawn(int team = -1, int x = -1, int y = -1) {
+		if( team == -1) { 
 			for( int i = 0; i < 4; i++ ) {
 				GameObject spawned = Instantiate(piecePrefab, floorArray[(i % 2) * (gameBoardSize - 1), (i / 2) * (gameBoardSize - 1)].transform.position, Quaternion.identity) as GameObject;
 				spawned.GetComponentInChildren<MeshRenderer>().material = floorArray[(i % 2) * (gameBoardSize - 1), (i / 2) * (gameBoardSize - 1)].GetComponent<MeshRenderer>().material;
-				spawned.tag = "team" + (i + 1).ToString();
+				spawned.tag = "team" + (i).ToString();
+				spawned.name = "team" + (i).ToString();
 			}
-		} else if(x < 0 || x >= gameBoardSize || y < 0 || y >= gameBoardSize)
-			throw new Exception("spawn cords are out of bounds!");
-		else {
-
+		} else if(team < 0 || team > 4)
+			Debug.LogError("Team Number not correct!");
+		else if( team != -1 && x != -1 && y != -1 ){
+			GameObject spawned = Instantiate(piecePrefab, floorArray[x, y].transform.position, Quaternion.identity) as GameObject;
+			spawned.tag = "team" + (team).ToString();
+			spawned.name = "team" + (team).ToString();
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// if( Input.GetKeyDown(KeyCode.W) ) {
-		// 	piece.transform.Translate(new Vector3(0.0f, 0.0f, 1.25f));
-		// } else if( Input.GetKeyDown(KeyCode.S) ) {
-		// 	piece.transform.Translate(-new Vector3(0.0f, 0.0f, 1.25f));
-		// } else if( Input.GetKeyDown(KeyCode.A) ) {
-		// 	piece.transform.Translate(-new Vector3(1.25f, 0.0f, 0.0f));
-		// } else if( Input.GetKeyDown(KeyCode.D) ) {
-		// 	piece.transform.Translate(new Vector3(1.25f, 0.0f, 0.0f));
-		// }
+		if( Input.GetKeyDown(KeyCode.W) ) {
+			foreach(GameObject obj in GameObject.FindGameObjectsWithTag("team1")) {
+				obj.transform.Translate(new Vector3(0.0f, 0.0f, 1.25f));
+			}
+		} else if( Input.GetKeyDown(KeyCode.S) ) {
+			foreach(GameObject obj in GameObject.FindGameObjectsWithTag("team1")) {
+				obj.transform.Translate(-new Vector3(0.0f, 0.0f, 1.25f));
+			}
+		} else if( Input.GetKeyDown(KeyCode.A) ) {
+			foreach(GameObject obj in GameObject.FindGameObjectsWithTag("team1")) {
+				obj.transform.Translate(-new Vector3(1.25f, 0.0f, 0.0f));
+			}
+		} else if( Input.GetKeyDown(KeyCode.D) ) {
+			foreach(GameObject obj in GameObject.FindGameObjectsWithTag("team1")) {
+				obj.transform.Translate(new Vector3(1.25f, 0.0f, 0.0f));
+			}
+		}
 	}
 }
